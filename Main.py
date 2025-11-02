85520ef290b0b99f8be5ea2da393652c0ae68eb7
Hamza <hamzaaboalzahab@gmail.com>
2025-11-02 21:21:41 +0300

    Completed The GUI , and added the functionality to each button

--- Main.py
+++ Main.py
@@ -1,30 +1,16 @@
 import cv2
 import numpy as np
 from typing import List
-from svgpathtools import Path, Line, CubicBezier, QuadraticBezier, wsvg
-from math import sin, pi
-from functools import lru_cache
 import subprocess
-import time
 import Svg_Converter
-
-def take_picture(imagepath : str):
-    cap = cv2.VideoCapture(0)
-    cap.set(3,480)
-    cap.set(4,620)
-    cap.set(10,100)
-    while True:
-        ret, frame = cap.read()
-        img = cv2.flip(frame , 1)
-        cv2.imshow("Camera", img)
-        cv2.waitKey(1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite(imagepath , frame)
-            cap.release()
-            cv2.destroyAllWindows()
-            break
-
-
+import time
+import pygame, sys
+from Button import Button
+from pygame import event
+from tkinter import Tk, filedialog
+import Svg_Converter
+import threading
+from grbl_uploader import GRBLUploader
 
 def center_gcode(lines, bed_width, bed_height):
     x_vals, y_vals = [], []
@@ -74,44 +60,333 @@ def center_gcode(lines, bed_width, bed_height):
 
 
 
-imgg = cv2.imread("C:/Users/pc/Desktop/CNC_plotter_photo/Docc.jpg")
-imagepath = "C:/Users/pc/Desktop/CNC_plotter_photo/Docc.jpg"
-cv2.imwrite(imagepath, imgg)
-outpath = "TestingImages/Processed.svg"
+def GcodeConvert(outPath : str) :
+    subprocess.run(
+        f"vpype --config \"C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/myconfig.toml\" read {outPath} translate 10mm 10mm scale 0.3mm 0.3mm linemerge linesort gwrite -p marlin_servo \"C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/meow.gc\"")
+    print(f"Gcode saved")
+
+    with open("C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/meow.gc") as f:
+        lines = f.readlines()
+
+    centered = center_gcode(lines, bed_width=297, bed_height=210)
+
+    with open("TestingImages/Centered.gc", "w") as f:
+        f.writelines(centered)
 
 
-SvgObject = Svg_Converter.SvgConverter(10 , 4)
-all_lines = SvgObject.SvgToSin(imagepath)
-all_lines = [p for p in all_lines if len(p) > 0]
-wsvg(paths=all_lines, filename=outpath)
-print(f"SVG saved as {outpath}")
-svg = f"C:/Users/pc/PycharmProjects/Cnc_Plotter/{outpath}"
-# Convert to gcode line
-subprocess.run(f"vpype --config \"C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/myconfig.toml\" read {svg} translate 10mm 10mm scale 0.3mm 0.3mm linemerge linesort gwrite -p marlin_servo \"C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/meow.gc\"")
-print(f"Gcode saved")
-with open("C:/Users/pc/PycharmProjects/Cnc_Plotter/TestingImages/meow.gc") as f:
-    lines = f.readlines()
 
-centered = center_gcode(lines, bed_width=297, bed_height=210)
+def upload_image():
+    root = Tk()
+    root.withdraw()
+    filepath = filedialog.askopenfilename(title="Select Image", filetypes=[("Image files", "*.png , *jpg , *jpeg")])
+    if filepath:
+        print(f"Selected image path {filepath}")
+        img = cv2.imread(filepath)
+        img = cv2.imwrite("./TestingImages/image.png", img)
 
-with open("TestingImages/Centered.gc", "w") as f:
-    f.writelines(centered)
-time.sleep(5)
+    else:
+        print("No image selected")
+        raise Exception("No image selected")
 
-from grbl_uploader import GRBLUploader
 
-if __name__ == "__main__":
+def take_picture():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 480)
+    cap.set(4, 620)
+    cap.set(10, 100)
+    while True:
+        ret, frame = cap.read()
+        img = cv2.flip(frame, 1)
+        cv2.imshow("Camera", img)
+        # cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite("./TestingImages/image.png", img)
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+Bg = pygame.image.load("assets/MainBack.png")
+BG = pygame.image.load("assets/Background.png")
+
+
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+
+def draw():
+    while True:
+        DRAW_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(80).render("Choose Your Way ", True, "#900000")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+        Draw_Capture = Button(image=pygame.image.load("assets/Camera.png"), pos=(640, 300), text_input="CAPTURE  ",
+                              font=get_font(55), base_color="White", hovering_color="Red")
+        Draw_UPLOAD = Button(image=pygame.image.load("assets/Upload.png"), pos=(640, 460),
+                             text_input=" UPLOAD FROM FILES   ", font=get_font(45), base_color="White",
+                             hovering_color="Red")
+        DRAW_BACK = Button(image=None, pos=(640, 570),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        DRAW_BACK.changeColor(DRAW_MOUSE_POS)
+        DRAW_BACK.update(SCREEN)
+        Draw_Capture.changeColor(DRAW_MOUSE_POS)
+        Draw_Capture.update(SCREEN)
+        Draw_UPLOAD.changeColor(DRAW_MOUSE_POS)
+        Draw_UPLOAD.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Draw_Capture.checkForInput(DRAW_MOUSE_POS):
+                    take_picture()
+                    print("Captured")
+                    style()
+            if event.type == pygame.MOUSEBUTTONUP:
+                if Draw_UPLOAD.checkForInput(DRAW_MOUSE_POS):
+                    imagePath = upload_image()
+
+                    print("UPLOAD FROM DEVICE")
+                    style()
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if DRAW_BACK.checkForInput(DRAW_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+
+def main_menu():
+    while True:
+        SCREEN.blit(Bg, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("CNC PLotter", True, "#900000")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                             text_input="DRAW", font=get_font(75), base_color="White", hovering_color="#900000")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="White", hovering_color="#900000")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="White", hovering_color="#900000")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    draw()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+def style():
+    while True:
+        Style_MOUSE_POS = pygame.mouse.get_pos()
+        SCREEN.blit(BG, (0, 0))
+        Style_Text = get_font(80).render("Choose Your Style", True, "#900000")
+        Style_RECT = Style_Text.get_rect(center=(640, 100))
+        SCREEN.blit(Style_Text, Style_RECT)
+        Squiggle_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(410, 400),
+                                 text_input="Squiggle ", font=get_font(55), base_color="Black",
+                                 hovering_color="#900000")
+        Scribble_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(850, 400),
+                                 text_input="Scribble ", font=get_font(55), base_color="Black",
+                                 hovering_color="#900000")
+        Style_BACK = Button(image=None, pos=(640, 570),
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        Style_BACK.changeColor(Style_MOUSE_POS)
+        Style_BACK.update(SCREEN)
+        Squiggle_Button.changeColor(Style_MOUSE_POS)
+        Squiggle_Button.update(SCREEN)
+        Scribble_Button.changeColor(Style_MOUSE_POS)
+        Scribble_Button.update(SCREEN)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Squiggle_Button.checkForInput(Style_MOUSE_POS):
+                    print("Squiggle")
+                    SquiggleA4()
+                    Run()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Scribble_Button.checkForInput(Style_MOUSE_POS):
+                    print("Scribble")
+                    Run()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Style_BACK.checkForInput(Style_MOUSE_POS):
+                    draw()
+        pygame.display.update()
+
+
+def SquiggleA4():
+    while True:
+        Squiggle_Mos_Pos = pygame.mouse.get_pos()
+        SCREEN.blit(BG, (0, 0))
+        Squiggle_Text = get_font(80).render("How Many Lines ?", True, "#900000")
+        Squiggle_RECT = Squiggle_Text.get_rect(center=(640, 100))
+        SCREEN.blit(Squiggle_Text, Squiggle_RECT)
+        first_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(450, 300), text_input="80",
+                              font=get_font(70), base_color="Black", hovering_color="#900000")
+        Second_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(830, 300), text_input="100",
+                               font=get_font(70), base_color="Black", hovering_color="#900000")
+        Third_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(450, 460), text_input="120",
+                              font=get_font(70), base_color="Black",
+                              hovering_color="#900000")
+        Fourth_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(830, 460), text_input="140",
+                               font=get_font(70), base_color="Black",
+                               hovering_color="#900000")
+        Style_BACK = Button(image=None, pos=(640, 570),
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        Style_BACK.changeColor(Squiggle_Mos_Pos)
+        Style_BACK.update(SCREEN)
+        first_Button.changeColor(Squiggle_Mos_Pos)
+        first_Button.update(SCREEN)
+        Second_Button.changeColor(Squiggle_Mos_Pos)
+        Second_Button.update(SCREEN)
+        Third_Button.changeColor(Squiggle_Mos_Pos)
+        Third_Button.update(SCREEN)
+        Fourth_Button.changeColor(Squiggle_Mos_Pos)
+        Fourth_Button.update(SCREEN)
+        imagePath = "TestingImages/image.png"
+        outPath = "./TestingImages/Processed.svg"
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if first_Button.checkForInput(Squiggle_Mos_Pos):
+                    Svg80 = Svg_Converter.SvgConverter(80, 4, White_Removal=False)
+
+                    Svg80.SvgToSin(imagePath, outPath)
+                    time.sleep(0.5)
+                    GcodeConvert(outPath)
+                    time.sleep(0.5)
+                    Run()
+                    print("First Button")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Second_Button.checkForInput(Squiggle_Mos_Pos):
+                    Svg100 = Svg_Converter.SvgConverter(100, 4, White_Removal=False)
+                    Svg100.SvgToSin(imagePath, outPath)
+                    time.sleep(0.5)
+                    GcodeConvert(outPath)
+                    time.sleep(0.5)
+                    Run()
+                    print("Second Button")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Third_Button.checkForInput(Squiggle_Mos_Pos):
+                    Svg120 = Svg_Converter.SvgConverter(120, 4, White_Removal=False)
+                    Svg120.SvgToSin(imagePath, outPath)
+                    time.sleep(0.5)
+                    GcodeConvert(outPath)
+                    time.sleep(0.5)
+                    Run()
+                    print("Third Button")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Fourth_Button.checkForInput(Squiggle_Mos_Pos):
+                    Svg140 = Svg_Converter.SvgConverter(140, 4, White_Removal=False)
+                    Svg140.SvgToSin(imagePath, outPath)
+                    time.sleep(0.5)
+                    GcodeConvert(outPath)
+                    time.sleep(0.5)
+                    Run()
+                    print("Fourth Button")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Style_BACK.checkForInput(Squiggle_Mos_Pos):
+                    style()
+        pygame.display.update()
+
+
+def Run():
     uploader = GRBLUploader(port="COM17")
     uploader.connect()
-    uploader.start_stream("TestingImages/Centered.gc")
+    while True:
+        Gcode_Mouse_Pos = pygame.mouse.get_pos()
+        SCREEN.blit(BG, (0, 0))
+        Gcode_Text = get_font(80).render("Running G-Code", True, "#900000")
+        Gcode_RECT = Gcode_Text.get_rect(center=(640, 100))
+        SCREEN.blit(Gcode_Text, Gcode_RECT)
+        Start_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 240), text_input="START",
+                              font=get_font(55), base_color="Black", hovering_color="#900000")
+        Pause_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 460), text_input="PAUSE",
+                              font=get_font(55), base_color="Black", hovering_color="#900000")
+        Resume_Button = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 350), text_input="RESUME",
+                               font=get_font(55), base_color="Black", hovering_color="#900000")
+        Pause_Button.changeColor(Gcode_Mouse_Pos)
+        Pause_Button.update(SCREEN)
+        Resume_Button.changeColor(Gcode_Mouse_Pos)
+        Resume_Button.update(SCREEN)
+        Start_Button.changeColor(Gcode_Mouse_Pos)
+        Start_Button.update(SCREEN)
 
-    # time.sleep(5)
-    # uploader.Pause()
-    # print("Paused...")
-    # time.sleep(3)
-    # uploader.resume()
-    # print("Resumed...")
 
-    # uploader.stop()  # Optional emergency stop
-    uploader.thread.join()  # Wait until upload finishes
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Pause_Button.checkForInput(Gcode_Mouse_Pos):
+                    print("Paused")
+                    uploader.Pause()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Resume_Button.checkForInput(Gcode_Mouse_Pos):
+                    uploader.resume()
+                    print("Resumed")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Start_Button.checkForInput(Gcode_Mouse_Pos):
+                    uploader.start_stream("TestingImages/Centered.gc")
+                    print("Started")
+        pygame.display.update()
+
+
+main_menu()
+
+
+
 
